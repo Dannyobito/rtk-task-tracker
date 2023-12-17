eea4460d495f8bdd6fcaaba69624b519b4333908
Dannyobito <dannyobito05@gmail.com>
2023-12-18 00:48:09 +0100

    added data persisting

--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
-import store from "./redux/store";
+import store, { persistor } from "./redux/store";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import { PersistGate } from "redux-persist/integration/react";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <PersistGate persistor={persistor}>
+        <App />
+      </PersistGate>
     </Provider>
+    ,
   </React.StrictMode>
 );
 
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,18 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import { persistReducer, persistStore } from "redux-persist";
 import tasksReducer from "../redux/tasks/tasksSlice";
+import persistConfig from "./persistConfig";
+
+const rootReducer = combineReducers({
+  tasks: tasksReducer,
+});
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 const store = configureStore({
-  reducer: {
-    tasks: tasksReducer,
-  },
+  reducer: persistedReducer,
 });
+
+export const persistor = persistStore(store);
 
 export default store;
